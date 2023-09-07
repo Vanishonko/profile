fc75053bfb4e398c55963625ce7c4c38a34ad15a
ivan iliev <vanishonko@gmail.com>
2023-09-07 23:11:26 +0200

    rewrote index + functioning navbar and CENTERED TEXT IN FOOTER

---
+++ js/sideNav.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openSideNav() {
+	document.getElementById("mySideNav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeSideNav() {
+	document.getElementById("mySideNav").style.width = "0";
+  }
