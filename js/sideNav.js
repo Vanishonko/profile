d75844b894e7eb4736ed011c5a88de7e7b85c908
Vanishonko <i.iliev23@acsbg.org>
2023-09-24 04:51:03 +0200

    improved navbar

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,9 +1,11 @@
 /* Open the sidenav */
 function openSideNav() {
-	document.getElementById("mySideNav").style.width = "20%";
-  }
-  
-  /* Close/hide the sidenav */
-  function closeSideNav() {
-	document.getElementById("mySideNav").style.width = "0";
-  }
+  document.getElementById("mySideNav").style.width = "20%";
+  document.getElementById("openSideNavBtn").style.opacity = "0";
+}
+
+/* Close/hide the sidenav */
+function closeSideNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  document.getElementById("openSideNavBtn").style.opacity = "1";
+}
